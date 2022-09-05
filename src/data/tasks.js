413581e50280e60b54472519b33c10ae900c3829
RomanPinelo <prof.roman.pinelo@gmail.com>
2022-09-05 10:11:53 -0500

    Update tasks.js

--- src/data/tasks.js
+++ src/data/tasks.js
@@ -1,17 +1,3 @@
 export const tasks = [
-  {
-    id: 0,
-    title: 'Mi primer tarea',
-    description: 'Mi primer tarea'
-  },
-  {
-    id: 1,
-    title: 'Mi segunda tarea',
-    description: 'Mi segunda tarea'
-  },
-  {
-    id: 2,
-    title: 'Mi tercer tarea',
-    description: 'Mi tercer tarea'
-  }
+  
 ];
