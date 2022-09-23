b46a404553a1d129b008a983b33dfa2c8bdc9f21
RomanPinelo <prof.roman.pinelo@gmail.com>
2022-09-23 14:34:42 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import TaskForm from "./components/TaskForm";
 
 function App() {
   return (
-    <main className="bg-zinc-900 h-screen">
+    <main className="bg-zinc-900 min-h-screen">
       <div className="container mx-auto p-10">
         <TaskForm/>
         <TaskList/>
